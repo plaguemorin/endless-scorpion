2a659cd43d55d701e3f60c1dc242a3a85e33dd70
Philippe Lague-Morin <philippe@screenshot.ca>
2015-07-15 17:54:10 -0400

    WIP

--- src/main/java/ca/screenshot/endlessscorpion/controllers/EventEndpoint.java
+++ src/main/java/ca/screenshot/endlessscorpion/controllers/EventEndpoint.java
@@ -1,8 +1,13 @@
 package ca.screenshot.endlessscorpion.controllers;
 
 import ca.screenshot.endlessscorpion.beans.ResponseBody;
+import oauth.signpost.OAuthConsumer;
+import oauth.signpost.exception.OAuthCommunicationException;
+import oauth.signpost.exception.OAuthExpectationFailedException;
+import oauth.signpost.exception.OAuthMessageSignerException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,7 +21,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/event")
 public class EventEndpoint {
-	private static final Logger LOG = LoggerFactory.getLogger(RestEndpoint.class);
+	private static final Logger LOG = LoggerFactory.getLogger(EventEndpoint.class);
+
+	@Autowired
+	private OAuthConsumer authConsumer;
 
 	@RequestMapping(method = RequestMethod.POST)
 	public String eventPost(@RequestBody final String token) {
@@ -28,7 +36,21 @@ public class EventEndpoint {
 	public ResponseBody eventGet(@RequestParam("url") final String url) {
 		final ResponseBody responseBody = new ResponseBody();
 		responseBody.setSuccess(true);
-		LOG.info("URL = {}", url);
+
+		// Process the event by getting the URL
+		// Normally, I'd put this in a JMS queue and have another
+		// worker process it so that we don't block the
+		// reply to the server, but dependencies are a pain
+		try {
+			final String signedUrl = this.authConsumer.sign(url);
+			LOG.info("Signed URL = {}", signedUrl);
+			
+		} catch (OAuthMessageSignerException | OAuthExpectationFailedException | OAuthCommunicationException e) {
+			LOG.error("Unable to sign message: {}", e);
+			responseBody.setSuccess(false);
+			responseBody.setMessage(e.getMessage());
+		}
+
 		return responseBody;
 	}
 }
